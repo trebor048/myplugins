dab09a9ba0187b64f0a44641afeddcc20b55492f
LuckyCanucky <88803142+trebor048@users.noreply.github.com>
2024-03-12 23:48:32 -0600

    doesnt work

--- AutoIdle/index.ts
+++ AutoIdle/index.ts
@@ -4,16 +4,14 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
 export default definePlugin({
     name: "** DOESNTWORK ** AutoIdleOnAFK",
     description: "changes your status to 'idle' wen you are tabbed out of discord for 5 minutes (originally created by RoguedBear but ported to Vencord)",
     authors: [
-        {
-            id: 904144997559975937n,
-            name: "Aero Solid Color",
-        },
+        Devs.lucky
     ],
     patches: [],
     // Delete these two below if you are only using code patches
